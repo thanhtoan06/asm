23f8c9178baca30b5fdd61ad81a6235599d1e557
thanhtoan06 <llthanhtoan@gmail.com>
2025-09-16 12:32:52 +0000

    first update

---
+++ test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void func(int x, int y)
+{
+    x = x + y;
+    printf("%d\n",x);
+}
+int main()
+{
+    int a=5;
+    int b=7;
+    func(a,b);
+    printf("Hello\n");
+    return 0;
+}
